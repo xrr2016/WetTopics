8a4d62816274697038445f609c5cb6c010060f91
xrr2016 <xiaoranran1993@163.com>
2017-04-11 22:20:56 +0800

    :zap: hello react

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import App from './App'
+import './index.css'
+
+class Header extends React.Component {
+  render () {
+    return (
+        <div>
+          <p>Cooooool!!!</p>
+        </div>
+    )
+  }
+}
+
 
 ReactDOM.render(
-  <App />,
+  <Header />,
   document.getElementById('root')
 );
