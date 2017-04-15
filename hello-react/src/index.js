1996b0f89c3fa3c8c5d4d67b60fd6be6f25908c7
xrr2016 <xiaoranran1993@163.com>
2017-04-15 22:33:41 +0800

    :art:

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 // import App from './App'
 import './index.css'
+
+class Title extends Component {
+  render = () => (
+    <h1>React Book!</h1>
+  )
+}
 
 class Header extends React.Component {
   render () {
     return (
         <div>
-          <p>Cooooool!!!</p>
+           <Title /> 
+           <p>This is Header</p>
         </div>
     )
   }
 }
 
+class Main extends Component {
+  render = () => (
+    <p>Main Content</p>
+  )
+}
+
+class Footer extends Component {
+  render = () => (
+    <p>This is footer</p>
+  )
+}
+
+class Index extends Component {
+  render = () => (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
 
 ReactDOM.render(
-  <Header />,
+  <Index />,
   document.getElementById('root')
 );
