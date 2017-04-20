27044f140f50aab52e166dacb967aac7d3398743
xrr2016 <xiaoranran1993@163.com>
2017-04-20 23:22:17 +0800

    :beers:

---
+++ redux-todos/src/store/reducer.js
@@ -0,0 +1,13 @@
+import { VisibilityFilters } from './actions'
+
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: []
+}
+
+function todoApp(state = initialState, action) {
+  // if (typeof state === 'undefined') {
+    // return initialState
+  // }
+  return state
+}
